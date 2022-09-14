3aedf47691d7becab996eb3f4402bbfe29e52cb7
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-09-13 23:14:13 -0300

    adding api

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a7a5fe2518f32f56bdfa390429ec2de6'),
+    },
+  },
+});
