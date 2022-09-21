964b4976abd6dba93dd0129f4a3352686b8b7e04
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-09-21 20:41:42 -0300

    Fixed overflow and created manytoMany method

--- api/admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
+++ api/admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const LeftMenuListLink = styled.div`
   max-height: 180px;
   margin-bottom: 19px;
-  overflow: auto;
+  overflow: hidden;
 `;
 
 export default LeftMenuListLink;
