2ac38ceb100ab51a08c466274563fe4bc440dbc0
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-10-10 12:37:30 -0300

    Add styles and animation to Menu

--- client/src/components/Menu/test.tsx
+++ client/src/components/Menu/test.tsx
@@ -1,13 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />)
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shoppingcart/i)).toBeInTheDocument()
+  })
 
-    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+  it('should render the open/close menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(container.firstChild).toMatchSnapshot()
+    //select the menu full
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // see if menu is hidden
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // click on the button and see if it opens
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //click on the button and to close menu and verify if its closed
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
