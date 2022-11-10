d6b981d9e019e74c0852ba59aefe28cbfb5faab9
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-11-10 02:57:41 -0300

    Connect mock data to the page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannerMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeHighlight: highlightMock,
+      freeGames: gamesMock
+    }
+  }
 }
