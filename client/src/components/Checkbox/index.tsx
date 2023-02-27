1ed91ff3a70cda0a89e0f5dfa39621a5b224dfb3
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-02-27 16:57:15 -0300

    Add dynamic values to the Checkbox

---
+++ client/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+export type CheckboxProps = {
+  label?: string
+  labelFor?: string
+}
+
+const Checkbox = ({ label, labelFor = '' }: CheckboxProps) => (
+  <S.Wrapper>
+    <input id={labelFor} type="checkbox" />
+    {!!labelFor && <label htmlFor={labelFor}>{label}</label>}
+  </S.Wrapper>
+)
+
+export default Checkbox
