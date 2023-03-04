a92cd262a2725d5ba15bc0687f1101b010eb5470
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-04 03:32:42 -0300

    Add Auth initial structure

---
+++ client/src/templates/Auth/test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
+  })
+})
