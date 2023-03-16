6f92ac69b774ce984e0b7257ba542ab59bc45744
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-16 18:46:33 -0300

    Add FormSignIn component and tests

---
+++ client/src/components/FormSignIn/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should the link forgot password', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+  })
+})
