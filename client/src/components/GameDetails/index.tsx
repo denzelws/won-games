b840fc55f098554403bdca51c28670127f9545f9
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-23 16:22:15 -0300

    Add GameDetails icons

--- client/src/components/GameDetails/index.tsx
+++ client/src/components/GameDetails/index.tsx
@@ -1,46 +1,67 @@
 import Heading from 'components/Heading'
 import MediaMatch from 'components/MediaMatch'
+
+import { Linux, Windows, Apple } from '@styled-icons/fa-brands'
+
 import * as S from './styles'
 
-const GameDetails = () => (
-  <S.Wrapper>
-    <MediaMatch greaterThan="small">
-      <Heading lineLeft lineColor="secondary">
-        Game Details
-      </Heading>
-    </MediaMatch>
+type Platform = 'linux' | 'windows' | 'mac'
 
-    <S.Content>
-      <S.Block>
-        <S.Label>Developer</S.Label>
-        <S.Description>GearBox Software</S.Description>
-      </S.Block>
+export type GameDetailsProps = {
+  platforms: Platform[]
+}
 
-      <S.Block>
-        <S.Label>Release Date</S.Label>
-        <S.Description>Nov 16, 2019 </S.Description>
-      </S.Block>
+const GameDetails = ({ platforms }: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux size={18} />,
+    mac: <Apple size={18} />,
+    windows: <Windows size={18} />
+  }
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
 
-      <S.Block>
-        <S.Label>Platforms</S.Label>
-      </S.Block>
+      <S.Content>
+        <S.Block>
+          <S.Label>Developer</S.Label>
+          <S.Description>GearBox Software</S.Description>
+        </S.Block>
 
-      <S.Block>
-        <S.Label>Publisher</S.Label>
-        <S.Description>2K</S.Description>
-      </S.Block>
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>Nov 16, 2019 </S.Description>
+        </S.Block>
 
-      <S.Block>
-        <S.Label>Rating</S.Label>
-        <S.Description>18+</S.Description>
-      </S.Block>
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
 
-      <S.Block>
-        <S.Label>Genius</S.Label>
-        <S.Description>Action / Adventure</S.Description>
-      </S.Block>
-    </S.Content>
-  </S.Wrapper>
-)
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>2K</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>18+</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Genius</S.Label>
+          <S.Description>Action / Adventure</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
 
 export default GameDetails
