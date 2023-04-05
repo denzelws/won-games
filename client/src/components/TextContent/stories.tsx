d78951b8366a311f1a88a6d5676cd357c6797200
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-04-05 11:34:03 -0300

    Add TextContent

---
+++ client/src/components/TextContent/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react'
+import TextContent, { TextContentProps } from '.'
+
+import textMock from './mock'
+
+export default {
+  title: 'TextContent',
+  component: TextContent,
+  args: textMock,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    },
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<TextContentProps> = (args) => (
+  <TextContent {...args} />
+)
---
+++ client/src/components/TextContent/test.tsx
@@ -0,0 +1,47 @@
+import { screen } from '@testing-library/react'
+
+import TextContent from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props = {
+  title: 'My Title',
+  content: `<h1>Content</h1>`
+}
+
+describe('<TextContent />', () => {
+  it('should render the title and content', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /my title/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /content/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(<TextContent content={props.content} />)
+
+    expect(
+      screen.queryByRole('heading', { name: /my title/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render the title and content with respective colors', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', {
+      name: /my title/i
+    }).parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
+  })
+})
