127591b27098b1fcb1933960009d71dfaae99b3c
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-04 15:51:07 -0300

    Add ProfileMenu styles and tests

---
+++ client/src/components/ProfileMenu/test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+
+import ProfileMenu from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+describe('<ProfileMenu />', () => {
+  it('should render the menu', () => {
+    const { container } = renderWithTheme(<ProfileMenu />)
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
