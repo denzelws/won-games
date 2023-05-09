661522bb14c3254335d71b4ff79e6f820f5e18a9
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-09 19:11:44 -0300

    Add ExploreSidebar component

---
+++ client/src/components/ExploreSidebar/index.tsx
@@ -0,0 +1,62 @@
+import Heading from 'components/Heading'
+import Checkbox from 'components/Checkbox'
+import Radio from 'components/Radio'
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+export type ItemProps = {
+  title: string
+  name: string
+  type: string
+  fields: Field[]
+}
+
+type Field = {
+  label: string
+  name: string
+}
+
+export type ExploreSidebarProps = {
+  items: ItemProps[]
+}
+
+const ExploreSidebar = ({ items }: ExploreSidebarProps) => (
+  <S.Wrapper>
+    {items.map((item) => (
+      <div key={item.title}>
+        <Heading lineBottom lineColor="secondary" size="small">
+          {item.title}
+        </Heading>
+
+        {item.type === 'checkbox' &&
+          item.fields.map((field) => (
+            <Checkbox
+              key={field.name}
+              name={field.name}
+              label={field.label}
+              labelFor={field.name}
+            />
+          ))}
+
+        {item.type === 'radio' &&
+          item.fields.map((field) => (
+            <Radio
+              key={field.name}
+              id={field.name}
+              value={field.name}
+              name={item.name}
+              label={field.label}
+              labelFor={field.name}
+            />
+          ))}
+      </div>
+    ))}
+
+    <Button fullWidth size="medium">
+      Filter
+    </Button>
+  </S.Wrapper>
+)
+
+export default ExploreSidebar
