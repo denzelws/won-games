dc9e3a5db7ebf1a3c900bd38eed69d5d0ea91bfa
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-12 13:19:34 -0300

    Add open/close mobile tests

--- client/src/components/ExploreSidebar/test.tsx
+++ client/src/components/ExploreSidebar/test.tsx
@@ -1,5 +1,7 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import { Overlay } from './styles'
+import { css } from 'styled-components'
 
 import items from './mock'
 
@@ -88,5 +90,30 @@ describe('<ExploreSidebar />', () => {
     userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
     expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
+  })
+
+  it('should open/close sidebar when filtering on mobile ', () => {
+    const { container } = renderWithTheme(
+      <ExploreSidebar items={items} onFilter={jest.fn} />
+    )
+
+    const variant = {
+      media: '(max-width:768px)',
+      modifier: String(css`
+        ${Overlay}
+      `)
+    }
+
+    const Element = container.firstChild
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/open filters/))
+
+    expect(Element).toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/close filters/))
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
   })
 })
