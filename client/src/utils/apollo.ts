80db6519719f536daf3a6d87f634380029fc4bc7
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-17 10:40:30 -0300

    Add apollo utils to work with SSR/SSG

---
+++ client/src/utils/apollo.ts
@@ -0,0 +1,38 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+import { useMemo } from 'react'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  // verify if exists, if not create another one
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  // restoring the data cache
+  if (initialState) {
+    apolloClientGlobal.restore(initialState)
+  }
+
+  // initializing at ssr with clean cache
+  if (typeof window === 'undefined') return apolloClientGlobal
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+  return store
+}
