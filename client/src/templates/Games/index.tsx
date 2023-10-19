02a35e01638e67b500e19e266bc6bf00512e4ada
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-10-19 13:04:41 -0300

    Connect ExploreSidebar to filter logic on Games page

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -9,6 +9,9 @@ import ExploreSidebar from 'components/ExploreSidebar'
 import * as S from './styles'
 import { ItemProps } from 'components/ExploreSidebar'
 import { Grid } from 'components/Grid'
+import { useRouter } from 'next/router'
+import { parseQueryStringToFilter, parseQueryStringToWhere } from 'utils/filter'
+import { ParsedUrlQueryInput } from 'querystring'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
@@ -16,11 +19,21 @@ export type GamesTemplateProps = {
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { query, push } = useRouter()
+
   const { data, loading, fetchMore } = useQueryGames({
-    variables: { limit: 15 }
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
   })
 
-  const handleFilter = () => {
+  const handleFilter = (items: ParsedUrlQueryInput) => {
+    push({
+      pathname: '/games',
+      query: items
+    })
     return
   }
 
@@ -31,7 +44,14 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
   return (
     <Base>
       <S.Main>
-        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        <ExploreSidebar
+          initialValues={parseQueryStringToFilter({
+            queryString: query,
+            filterItems
+          })}
+          items={filterItems}
+          onFilter={handleFilter}
+        />
 
         {loading ? (
           'Loading...'
