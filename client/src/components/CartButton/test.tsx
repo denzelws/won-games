c4960432b6c18ff0e4087c007b4d9c49d8149d64
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-10-25 13:12:50 -0300

    Add CartButton

---
+++ client/src/components/CartButton/test.tsx
@@ -0,0 +1,36 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
+
+import Button from '.'
+import { fireEvent, render, screen } from 'utils/test-utils'
+
+describe('<CartButton/>', () => {
+  it('should render button to add and call method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => false,
+      addToCart: jest.fn()
+    }
+
+    render(<Button id="1" />, { cartProviderProps })
+    const button = screen.getByLabelText(/add to cart/i)
+    expect(button).toBeInTheDocument()
+
+    fireEvent.click(button)
+    expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
+  })
+
+  it('should render button to remove and call method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+
+    render(<Button id="1" />, { cartProviderProps })
+    const button = screen.getByLabelText(/remove from cart/i)
+    expect(button).toBeInTheDocument()
+
+    fireEvent.click(button)
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
+  })
+})
