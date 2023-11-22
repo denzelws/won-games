0d128168be253fabe005bd83e5a2ee374e04da08
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-11-21 21:12:06 -0300

    Configure MSW

---
+++ client/.jest/server-mock.ts
@@ -0,0 +1,17 @@
+import {server} from '../src/utils/mockServer/server'
+
+beforeAll(() => {
+  // fica escutando todas as chamadas nos testes
+  server.listen()
+})
+
+afterEach(() => {
+  // reseta todos os handlers para caso ele seja chamado
+  // de novo
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  //fecha o server e limpa os testes
+  server.close()
+})
