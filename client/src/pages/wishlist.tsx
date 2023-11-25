2a02e4ac0e88634b46542c2e05442e3d30821c87
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-11-25 02:25:23 -0300

    Add Wishlist Page

--- client/src/pages/wishlist.tsx
+++ client/src/pages/wishlist.tsx
@@ -9,6 +9,11 @@ import gamesMock from 'components/GameCardSlider/mock'
 
 import { GetServerSidePropsContext } from 'next'
 import protectedRoutes from 'utils/protected-routes'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
@@ -16,13 +21,24 @@ export default function WishlistPage(props: WishlistTemplateProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo(null, session)
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
   })
+
+  if (!session) return {}
+
+  await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: session.user?.email as string
+    }
+  })
+
   return {
     props: {
       session,
+      initializeApolloState: apolloClient.cache.extract(),
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedTitle: data.recommended?.section?.title,
       upcomingHighlight: highlightMapper(data.recommended?.section?.highlight),
