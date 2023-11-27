1bbb3d09a810c2a840537255808c97fb0b8abf0b
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-11-26 21:05:35 -0300

    Add error handler from Strapi

--- client/src/components/PaymentForm/index.tsx
+++ client/src/components/PaymentForm/index.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import { CardElement } from '@stripe/react-stripe-js'
-import { ShoppingCart } from '@styled-icons/material-outlined'
+import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 
 import Heading from 'components/Heading'
 import Button from 'components/Button'
 
 import * as S from './styles'
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>(null)
+
+  const handleChange = (event: StripeCardElementChangeEvent) => {
+    setError(event.error ? event.error?.message : '')
+  }
+
   return (
     <S.Wrapper>
       <S.Body>
         <Heading color="black" size="small" lineBottom>
           Payment
         </Heading>
-        <CardElement options={{ hidePostalCode: true }} />
+        <CardElement
+          options={{ hidePostalCode: true }}
+          onChange={handleChange}
+        />
+        {error && (
+          <S.Error>
+            <ErrorOutline size={20} /> {error}
+          </S.Error>
+        )}
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
--- client/src/components/PaymentForm/styles.ts
+++ client/src/components/PaymentForm/styles.ts
@@ -28,3 +28,13 @@ export const Footer = styled.div`
     }
   `}
 `
+
+export const Error = styled.p`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+    padding-top: ${theme.font.sizes.xsmall};
+  `}
+`
