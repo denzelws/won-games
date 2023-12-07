b272fa2af2e739ee4d71dd4859e0ae7b3f9a794e
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-12-06 21:03:06 -0300

    Config Cypress TS

---
+++ client/cypress/support/index.d.ts
@@ -0,0 +1,12 @@
+// load type definitions from cypress module
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to visit google
+     * @example cy.google()
+     */
+    google(): Chainable<Window>
+  }
+}
