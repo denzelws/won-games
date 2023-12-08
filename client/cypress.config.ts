fc9f2b79b4804c377383df7755a1f3b9ab0f166e
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-12-08 17:55:17 -0300

    Add baseUrl to Cypress

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -6,8 +6,10 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       return require('./cypress/plugins/index.js')(on, config)
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
+    baseUrl: 'http://localhost:3000'
+  }
 })
