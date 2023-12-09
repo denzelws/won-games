267440fe815b1923a72b1038c00c2fd03d0adff0
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-12-09 11:12:59 -0300

    Start getting banner with get, findByRole and within

--- client/cypress/e2e/home.cy.ts
+++ client/cypress/e2e/home.cy.ts
@@ -3,5 +3,10 @@
 describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/');
+
+    cy.get('.slick-slider').first().within(() => {
+      cy.findByRole('heading', { name: 'Cyberpunk 2077'} )
+      cy.findByRole('link', { name: /buy now/i })
+    })
   })
 })
