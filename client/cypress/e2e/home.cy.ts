31f022b06647fd1510a5f1f3799abd58bc7874ab
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-12-09 19:02:45 -0300

    Create shouldRenderShowcase custom command and use of assertions

--- client/cypress/e2e/home.cy.ts
+++ client/cypress/e2e/home.cy.ts
@@ -5,5 +5,8 @@ describe('Home Page', () => {
     cy.visit('/');
 
     cy.shouldRenderBanner()
+    cy.shouldRenderShowcase({name: "New Games" })
+    cy.shouldRenderShowcase({name: "Most Popular Games" })
+    cy.shouldRenderShowcase({name: "Upcoming Games" })
   })
 })
