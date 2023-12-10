7f7811b83a051490ffcb99535afb64c4368df8be
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-12-10 16:38:50 -0300

    Refactor to create getByDataCy custom command

--- client/cypress/support/index.d.ts
+++ client/cypress/support/index.d.ts
@@ -8,16 +8,24 @@ type ShowcaseAttributes = {
 
 declare namespace Cypress {
   interface Chainable {
-      /**
-     * Custom command to check banner in page
-     * @example cy.shouldRenderBanner()
-     */
+
+     /**
+    * Custom command to get element by data-cy
+    * @example cy.getByDataCy('selector')
+    */
+    getByDataCy(selector: string): Chainable<JQuery<HTMLElement>>
+
+     /**
+    * Custom command to check banner in page
+    * @example cy.shouldRenderBanner()
+    */
       shouldRenderBanner(): Chainable<Element>
 
-       /**
-     * Custom command to check showcase in page
-     * @example cy.shouldRenderShowcase()
-     */
+     /**
+    * Custom command to check showcase in page
+    * @example cy.shouldRenderShowcase()
+    */
        shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
+
   }
 }
