83ffad181ae2f97d6e92a6518c962def208310a9
DenzelWashington <denzelwashingtonsantana@gmail.com>
2024-01-10 04:12:56 -0300

    Disable ssl at database production config

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -10,10 +10,9 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL') ? {
-          require: true,
+        ssl: {
           rejectUnauthorized: false,
-        } : false,
+        }
       },
       options: {}
     },
