6b616ac0a47a16f6f2d42359417cd8f72536f21a
DenzelWashington <denzelwashingtonsantana@gmail.com>
2024-01-10 16:49:02 -0300

    Trying to fix production

--- api/config/env/production/database.js
+++ api/config/env/production/database.js
@@ -1,25 +1,23 @@
 const parse = require('pg-connection-string').parse;
-//const config = parse(process.env.DATABASE_URL);
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
         ssl: {
           rejectUnauthorized: false,
         },
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
     },
   },
 });
